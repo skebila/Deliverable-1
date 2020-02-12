806976c9cfd58f870f20c7a05fbb26deb8746e17
perepeli <59895770+perepeli@users.noreply.github.com>
2020-02-11 19:08:58 -0500

    Documentation updates

--- Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Game.java
+++ Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Game.java
@@ -1,22 +1,34 @@
 /**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
+ * 1201_4081 Project Winter 2020 Base code.
+ * @author daniel, 2020
  */
-package ca.sheridancollege.project;
+
+package CardGame;
+
+/**
+ * The class that models a game, where everything will happen. AKA "main" class
+ * Here we will declare players, their hands, game deck some game mechanics, and so on...
+ * We should create a more specific child of this class and instantiate the methods given.
+ */
 
 import java.util.ArrayList;
 
-/**
- * The class that models your game. You should create a more specific
- * child of this class and instantiate the methods given.
- * @author dancye, 2018
- */
-public abstract class Game 
+public abstract class Game
 {
+    /**
+     * @param gameName The title of the game, - "UNO"
+     */
     private final String gameName;//the title of the game
+    /**
+     * @param players A set of (Players) for our game, who will be taking a part.
+     */
     private ArrayList <Player> players;// the players of the game
-    
+
+    /**
+     * A default constructor which will be overriding.
+     * Here we should have a specified number of players for setting.
+     * @param givenName the name of our game static value "UNO"
+     */
     public Game(String givenName)
     {
         gameName = givenName;
@@ -26,39 +38,40 @@ public abstract class Game
     /**
      * @return the gameName
      */
-    public String getGameName() 
+    public String getGameName()
     {
         return gameName;
     }
-    
-     /**
-     * @return the players of this game
+
+    /**
+     * @return the players of this game represented as (toString) in the future implementation
      */
-    public ArrayList <Player> getPlayers() 
+    public ArrayList <Player> getPlayers()
     {
         return players;
     }
 
     /**
-     * @param players the players of this game
+     * @param players the players of this game, either should be declared before the game,
+     * in the class game constructor, or we need to create a more suitable solution for us.
      */
-    public void setPlayers(ArrayList <Player> players) 
+    public void setPlayers(ArrayList <Player> players)
     {
         this.players = players;
     }
-    
+
     /**
      * Play the game. This might be one method or many method calls depending
      * on your game.
      */
     public abstract void play();
-    
+
     /**
      * When the game is over, use this method to declare and display a winning
      * player.
      */
     public abstract void declareWinner();
 
-   
-    
+
+
 }//end class
