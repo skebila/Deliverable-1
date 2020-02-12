aa0d2dda5c60e1005411875f46f60089e9f0fd0e
perepeli <59895770+perepeli@users.noreply.github.com>
2020-02-11 19:05:32 -0500

    Changes for with documentations

--- Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
+++ Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
@@ -1,26 +1,36 @@
 /**
- * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
+ * 1201_4081 Project Winter 2020 Base code.
+ * @author daniel, 2020
  */
-package ca.sheridancollege.project;
+
+package CardGame;
 
 /**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
- * @author dancye, 2018
+ * An abstract class "Card" for the future UNO Card game project.
+ * Will be working as an object inside (GroupOfCards) -> |LinkedList| to represent a deck
+ * @author daniel, 2020
  */
-public abstract class Card 
+
+public abstract class Card
 {
-    //default modifier for child classes
-    
+    public Enum Value; // ENUM with all the possible values of UNO cards
+    public Enum Suit; // ENUM with all the possible suits of UNO cards
+
     /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     * A base constructor, to set a value and suit of an object of this class
+     * need to create these fields in a real implementation of this class,
+     * there is no need not to make them final, so we dont need setters in this class.
      */
-    
+
+    public Card() {
+        // This constructor should set our final values
+    }
+
+    /**
+     * @return a String representation of a UNO card.
+     */
+
     @Override
-    public abstract String toString();
-    
+    public abstract String toString(); // String representation of a card
+
 }
